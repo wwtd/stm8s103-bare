3ea8ece6b1b3ed41472edb28fe71f4e8c4757af9
wwtd <51043256+wwtd@users.noreply.github.com>
2025-06-25 21:25:02 +0800

    driver ws2812

--- 04-ws2812-drv/src/main.c
+++ 04-ws2812-drv/src/main.c
@@ -7,6 +7,8 @@ extern void uart1_init(void);
 extern void enable_HSI_16MHz(void);
 extern void on_board_led_init(void);
 extern void on_board_led_blink(void);
+extern void ws2812_init(void);
+extern void ws2812_refresh(void);
 
 
 void main()
@@ -14,9 +16,11 @@ void main()
     enable_HSI_16MHz();
     on_board_led_init();
     uart1_init();
+    ws2812_init();
     while (1)
     {
-        printf("hello\r\n");
+        // printf("hello\r\n");
+        ws2812_refresh();
         faker_delay(10000);
     }
 }
--- 04-ws2812-drv/src/ws2812-drv.c
+++ 04-ws2812-drv/src/ws2812-drv.c
@@ -2,13 +2,9 @@
 #include "stdint.h"
 
 #define WS2812_SERIAL_NUM       (16U)
+#define WS2812_CONTRL_BIT       (24U)
 static uint32_t gs_ws2812_config[WS2812_SERIAL_NUM] = {0};
 
-void ws2812_inint(void)
-{
-    GPIO_D_BASE_ADDR->DDR |= (1 << 4);
-    GPIO_D_BASE_ADDR->CR1 |= (1 << 4);
-}
 
 #define ws2812_send_1 {                                                   \
   __asm__("bset	20495, #4");                                       \
@@ -32,13 +28,58 @@ void ws2812_inint(void)
   );                                                               \
 }
 
-void send_raw_color(uint32_t color) {
-  for(int i=0; i<24; i++) {
-    if(color & 1) {
+#define ws2812_reset {                                              \
+  __asm__("bres	20495, #4");                                       \
+  __asm__(                                                         \
+    "nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\n"           \
+  );                                                               \
+  __asm__(                                                         \
+    "nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\n"           \
+  );                                                               \
+  __asm__(                                                         \
+    "nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\n"           \
+  );                                                               \
+  __asm__(                                                         \
+    "nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\n"           \
+  );                                                               \
+  __asm__(                                                         \
+    "nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\n"           \
+  );                                                               \
+  __asm__(                                                         \
+    "nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\n"           \
+  );                                                               \
+  __asm__(                                                         \
+    "nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\n"           \
+  );                                                               \
+  __asm__(                                                         \
+    "nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\n"           \
+  );                                                               \
+  __asm__(                                                         \
+    "nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\n"           \
+  );                                                               \
+  __asm__(                                                         \
+    "nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\n"           \
+  );\
+}
+
+void ws2812_init(void)
+{
+    GPIO_D_BASE_ADDR->DDR |= (1 << 4);
+    GPIO_D_BASE_ADDR->CR1 |= (1 << 4);
+}
+
+void ws2812_refresh(void)
+{
+  for(uint16_t i = 0; i< WS2812_CONTRL_BIT * WS2812_SERIAL_NUM; ++i)
+  {
+    if((gs_ws2812_config[i/WS2812_CONTRL_BIT] >> (i%WS2812_CONTRL_BIT)) & 0x1)
+    {
       ws2812_send_1;
-    } else {
+    }
+    else
+    {
       ws2812_send_0;
     }
-    color >>= 1;
   }
+  ws2812_reset;
 }
